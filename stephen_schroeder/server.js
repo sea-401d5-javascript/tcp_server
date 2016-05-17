e65e7bf4d7a146c41ff1361156b76e81f1ffc6a6
Stephen Schroeder <schrode50@gmail.com>
2016-05-17 12:40:32 -0700

    pushing for submission

--- stephen_schroeder/server.js
+++ stephen_schroeder/server.js
@@ -1,16 +1,20 @@
 const net = require('net');
 
-const sockets = [];
+const users = [];
 
-net.createServer((socket) => {
-  sockets.push(socket);
-  socket.on('data', (chunk) => {
-    console.log(chunk.toString());
-    socket.write('MESSAGE RECEIVED');
-    sockets.forEach((s) => {
-      s.write('BROADCASTING: ' + chunk.toString());
+module.exports = function() {
+  net.createServer((socket) => {
+    socket.name = socket.remotePort + new Date();
+    users.push(socket);
+    socket.on('data', (chunk) => {
+      console.log(chunk.toString());
+      users.forEach((s) => {
+        if (socket.name !== s.name) {
+          s.write(socket.name + ': ' + chunk.toString());
+        }
+      });
     });
+  }).listen(3000, () => {
+    console.log('listening on port 3000');
   });
-}).listen(3000, () => {
-  console.log('listening on port 3000');
-});
+};
