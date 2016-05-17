6ad1f45ee7fae5d332e45089a449da94c5ee7391
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-17 16:16:18 -0700

    created new variable

--- aliza/test/test.js
+++ aliza/test/test.js
@@ -5,14 +5,23 @@ const client = require('../tcp_client');
 
 describe('tcp tests', () => {
   it('should send message to all clients, but not to sender', (done) => {
+    var result;
+    var wroteBack = false;
     var firstClient = net.connect(3000, () => {
+      firstClient.on('data', () => {
+        wroteBack = true;
+      })
       firstClient.write('message');
     })
     var secondClient = net.connect(3000, () => {
       secondClient.on('data', (data) => {
-        expect(data.toString()).to.eql('message');
-        done();
+        result = data.toString();
       })
-    })
+    });
+    setTimeout(() => {
+      expect(result).to.eql('message');
+      expect(wroteBack).to.eql(false);
+      done();
+    }, 50)
   })
 });
