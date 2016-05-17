b46d33d3247c7d7b205a1fc3b12a3bdc15836b50
Chris Perez <chris.k.perez@gmail.com>
2016-05-17 16:18:09 -0700

    better testing

--- test/test.js
+++ test/test.js
@@ -3,15 +3,26 @@
 const expect = require('chai').expect;
 const net = require('net');
 const server = require(__dirname + '/../lib/tcp-server');
-const client1 = net.connect({port:3000});
-const client2 = net.connect({port:3000});
 
-describe('', ()=>{
-  it('should send a message', ()=>{
-    client1.write('things')
-    expect();
-  });
-  it('should ', ()=>{
-    expect();
+describe('tcp tests', ()=>{
+  it('should send a message to all clients except sender', (done)=>{
+    let result;
+    let wroteBack = false;
+    const client1 = net.connect(3000, ()=>{
+      client1.on('data', (data)=>{
+        if (data != 'SENDING... \n') wroteBack = true;
+      })
+      client1.write('testing');
+    });
+    const client2 = net.connect(3000, ()=>{
+      client2.on('data', (data)=>{
+        result = data.toString();
+      });
+    });
+    setTimeout(()=>{
+      expect(result.slice(-7)).to.eql('testing');
+      expect(wroteBack).to.be.false;
+      done();
+    }, 200)
   });
 });
