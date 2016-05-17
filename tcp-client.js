63e526276bd68543c1219bd975185771600a6058
Dan Stineback <dstineback@gmail.com>
2016-05-17 11:25:46 -0700

    final save before testing

--- tcp-client.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const net = require('net');
-
-const client = net.connect(3000, () => {
-  client.on('data', (data) => {
-    client.write('Client One: ');
-  })
-  client.write('Message recieved');
-});
--- tcp-server.js
+++ tcp-server.js
@@ -5,22 +5,24 @@ const fs = require('fs');
 const sockets = [];
 
 const server = net.createServer((socket) => {
-  socket.push = sockets;
-
-  // var response = chunk.toString();
-  // var date = new Date();
-  // fs.writeFile(__dirname + '/response/' + date.toString() + '.txt', response);
-  // console.log(response);
+  sockets.push(socket);
 
   socket.on('data', (chunk) => {
-    socket.write('message Recieved: ')
+
+    var response = chunk.toString();
+    var date = new Date();
+
+    console.log(response + date);
+
+    socket.write('SENDING.. \n');
     sockets.forEach((s) => {
-      s.write('Message Recieved: ' + chunk.toString());
+      if (s != socket) s.write('RECIEVING.. \n' + chunk.toString());
     });
+
   });
   socket.on('end', () => {
     console.log('socket end');
-  })
+  });
 }).listen(3000, () => {
   console.log('Up on 3000');
 });
