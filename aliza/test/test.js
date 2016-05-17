99a506aba59cd868de52d715c8aa6999a55a36cb
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-17 13:48:19 -0700

    figuring out testing

--- aliza/test/test.js
+++ aliza/test/test.js
@@ -3,17 +3,16 @@ const expect = require('chai').expect;
 const server = require('../tcp_server');
 const client = require('../tcp_client');
 
-describe('client receiving same message that was sent form other client', () => {
-  it('should receive the same message that was sent', (done) => {
+describe('tcp tests', () => {
+  it('should send message to all clients, but not to sender', (done) => {
     var firstClient = net.connect(3000, () => {
       firstClient.write('message');
     })
     var secondClient = net.connect(3000, () => {
-      secondClient.on('data', (chunk) => {
-        console.log(chunk.toString());
+      secondClient.on('data', (data) => {
+        expect(data.toString()).to.eql('message');
+        done();
       })
     })
-    expect('message').to.eql(chunk.toString());
-    done();
-  });
+  })
 });
