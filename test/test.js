45c3f32310eecb9ea98ef81cd95a34bad1bd6761
jdar22169 <mmow313@gmail.com>
2016-05-17 20:00:06 -0700

    got the test to work using class notes

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+'use strict'
 const expect = require('chai').expect;
 const net = require('net');
 const server = require('../tcp_server.js');
@@ -5,15 +6,25 @@ const server = require('../tcp_server.js');
 
 describe('Test TCP server', () => {
   it('message received is equal to message sent', (done) => {
+    let result;
+    let wroteback = false;
     const client1 = net.connect(3000, () => {
+      client1.on('data', () => {
+        wroteback = true;
+        });
       client1.write('Hello')
-    })
+      })
     const client2 = net.connect(3000, () => {
       client2.on('data', (message) => {
-        expect('Username: ' + server.name + '--->' + 'Hello').to.eql(message.toString());
-        done();
-      })
-    })
+        result = (message.toString()).slice(-5);
+      });
+    });
+
+    setTimeout(() => {
+      expect(result).to.eql('Hello');
+      expect(wroteback).to.eql(false);
+      done();
+      }, 50);
 
     });
 })
