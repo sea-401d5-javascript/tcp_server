ae9f2ac650ce64d24184b9ad1dc85de4d98172cc
Ruben Sarkisyan <rubens24@uw.edu>
2016-05-19 11:29:28 -0700

    made changes

--- tcp-server/tcp-client.js
+++
@@ -1,8 +0,0 @@
-const net = require('net');
-
-const client = net.connect(3000, () => {
-  client.on('data', (data) => {
-    client.write('MESSAGE RECIEVED');
-  })
-  client.write('HERE IS MESSAGE');
-})
